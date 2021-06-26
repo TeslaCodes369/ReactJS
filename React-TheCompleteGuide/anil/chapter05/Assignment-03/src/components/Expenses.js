793582fad7079338164b4ae64d38f5acb7fb973c
Anil Kumar <anil.kumar4@hp.com>
2021-06-26 15:26:59 +0530

    Assign03

---
+++ React-TheCompleteGuide/anil/chapter05/Assignment-03/src/components/Expenses.js
@@ -0,0 +1,33 @@
+import "./Expenses.css";
+import { useState } from "react";
+import Card from "./Card";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+
+const Expenses = (props) => {
+  const [selectedYear, setSelectedYear] = useState("2019");
+
+  const onExpenseYearSelected = (year) => {
+    setSelectedYear(year);
+  };
+
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === selectedYear;
+  });
+
+  return (
+    <li>
+      <Card className="expenses">
+        <ExpensesFilter
+          onChangeSelectedYear={onExpenseYearSelected}
+          selectedYear={selectedYear}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </li>
+  );
+};
+
+export default Expenses;
